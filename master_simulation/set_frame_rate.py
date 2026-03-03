de489fd0fa7dc855207b884f7cf01f2fc65f3424
dnmaulana1907 <danimaulana1907@gmail.com>
2026-03-03 12:10:02 +0700

    file to set frame rate

---
+++ master_simulation/set_frame_rate.py
@@ -0,0 +1,7 @@
+from command_list import PROCESS_FAIL, set_frame_rate
+
+SLAVE_ID = b'\x01'
+FRAME_RATE = 100
+
+if set_frame_rate(SLAVE_ID, FRAME_RATE) == PROCESS_FAIL:
+    print("Failed to set frame rate on the sensor.")
